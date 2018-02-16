e4f2fd2d60aefe856a10f67e5353e740d3e680e5
ahulsem <aaron.hulseman@outlook.com>
2018-02-15 18:05:26 -0600

    new node modules and updates to app.js and classify.js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require("body-parser");
+var multer = require("multer");
 var formidable = require('formidable');
 var fs = require('fs');
+var sharp = require('sharp');
+var readChunk = require('read-chunk');
+var fileType = require('file-type');
+
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 //var http = require('http');
 //var url = require('url');
-var fileName = 'CharlieBrown.png';
-var response = 'Info on Charlie Brown';
 
 var classify = require('./routes/classify');
 
@@ -14,7 +23,15 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/classify', classify);
+app.get('/classify', classify);
+
+app.get('/testimage', classify);
+
+
+app.post('/store', function(req, res){
+    console.log(req.method);
+    res.redirect("/uploads");
+});
 
 app.get('/', function(req, res){
     console.log(req.method);
@@ -29,22 +46,48 @@ app.get('/classify', function(req, res){
 });
 */
 
+/*app.post("/uploads", multer({dest: "./uploads/"}).array("uploads", 12), function(req, res) {
+    var fileInfo = [];
+    for(var i = 0; i < req.files.length; i++) {
+        fileInfo.push({
+            "originalName": files[i].originalName,
+            "size": file.size,
+            "b64": new Buffer(fs.readFileSync(file.path)).toString("base64")
+        });
+        fs.unlink(req.files[i].path);
+    }
+    res.send(fileInfo);
+});*/
+
+
 app.post('/uploads', function(req, res){
     console.log(req.method);
 
     // create an incoming form object
     var form = new formidable.IncomingForm();
-
+    
     // specify that we want to allow the user to upload multiple files in a single request
-    form.multiples = true;
+    //form.multiples = true;
 
     // store all uploads in the /uploads directory
     form.uploadDir = path.join(__dirname, '/uploads');
 
     // every time a file has been uploaded successfully,
     // rename it to it's orignal name
-    form.on('file', function(field, file) {
-    fs.rename(file.path, path.join(form.uploadDir, file.name));
+    form.on('file', function(name, file) {
+        
+        // Log file metadata to console
+        console.log(file.name);
+        console.log(file.type);
+        console.log(file.size);
+        
+        // Save original image to /uploads
+        fs.rename(file.path, path.join(form.uploadDir, file.name));
+        
+        // Use sharp to convert image and save it /uploads/jpegs
+        sharp('uploads/'+file.name).toFile("uploads/jpegs/Charlie_Brown.jpg", function(err, info){
+            if(err) console.log(err);
+        });
     });
 
     // log any errors that occur
@@ -59,9 +102,11 @@ app.post('/uploads', function(req, res){
 
     // parse the incoming request containing the form data
     form.parse(req);
+    
 
 });
 
 var server = app.listen(port, function(){
   console.log('Server listening on port 8080');
 });
+
--- routes/classify.js
+++ routes/classify.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var readChunk = require('read-chunk');
+var fileType = require('file-type');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,5 +17,19 @@ router.get('/classify', function(req, res, next){
     antInfo = "https://www.antweb.org/description.do?genus=" + genus + "&species=" + species;
     res.send(antInfo);
 });
+
+function validateImage(){
+    // Validate image using file-type
+    var buffer = readChunk.sync('uploads/'+file.name, 0, 4100);
+
+    console.log(fileType(buffer));
+    var imgType = fileType(buffer).ext;
+
+    // Check file type to convert to jpeg.
+    if(imgType.valueOf() == 'jpg'.valueOf | imgType.valueOf() =='png'.valueOf | imgType.valueOf() =='gif'.valueOf | imgType.valueOf() =='jpeg'.valueOf | imgType.valueOf() =='bmp'.valueOf | imgType.valueOf() =='tif'.valueOf | imgType.valueOf() =='tiff'.valueOf | imgType.valueOf() =='raw'.valueOf | imgType.valueOf() =='wmf'.valueOf | imgType.valueOf() =='webp'.valueOf | imgType.valueOf() =='img'.valueOf | imgType.valueOf() =='pct'.valueOf | imgType.valueOf() =='tga'.valueOf | imgType.valueOf() =='jpe'.valueOf | imgType.valueOf() =='ani'.valueOf | imgType.valueOf() =='heif'.valueOf | imgType.valueOf() =='heic'.valueOf){
+        return true;
+    } 
+    else return false;
+};
 
 module.exports = router;
